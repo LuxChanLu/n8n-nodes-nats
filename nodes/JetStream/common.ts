71bad98a53555c1986296976011c9b1226cc4f5e
zetanova <dirnberger@cwtech.at>
2023-11-28 14:48:52 +0100

    share nats connection option factory

--- nodes/JetStream/common.ts
+++ nodes/JetStream/common.ts
@@ -1,5 +1,6 @@
 import { IAllExecuteFunctions } from "n8n-workflow";
 import { JetStreamClient, JetStreamOptions, NatsConnection, connect } from "nats";
+import { natsConnectionOptions } from "../common";
 
 interface JSConnection {
 	nats: NatsConnection
@@ -8,12 +9,13 @@ interface JSConnection {
 
 export const jsNatsConnection = async (func: IAllExecuteFunctions, idx: number): Promise<JSConnection>  => {
 	const credentials = await func.getCredentials('natsApi', idx)
-	const nats = await connect(credentials)
-	const jsOptions = {
+	const options = natsConnectionOptions(credentials)
+	const nats = await connect(options)
+	const jsOptions:JetStreamOptions = {
 		apiPrefix: credentials['jsApiPrefix'] as string,
     timeout: credentials['jsTimeout'] as number,
     domain: credentials['jsDomain'] as string
-	} as JetStreamOptions;
+	}
 	if (jsOptions.apiPrefix === '') {
 		jsOptions.apiPrefix = undefined
 	}
