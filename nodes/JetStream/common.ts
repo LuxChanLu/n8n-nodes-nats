d67fe6cc67e639d4b9ae03fd4c4578332f66993b
Hugo Meyronneinc <hugo@lxc.lu>
2023-11-28 21:33:50 +0100

    Merge pull request #2 from Zetanova/master

--- nodes/JetStream/common.ts
+++ nodes/JetStream/common.ts
@@ -1,5 +1,7 @@
 import { IAllExecuteFunctions } from "n8n-workflow";
 import { JetStreamClient, JetStreamOptions, NatsConnection, connect } from "nats";
+import { natsConnectionOptions } from "../common";
+import { defaultJsOptions } from "nats/lib/jetstream/jsbaseclient_api";
 
 interface JSConnection {
 	nats: NatsConnection
@@ -8,17 +10,20 @@ interface JSConnection {
 
 export const jsNatsConnection = async (func: IAllExecuteFunctions, idx: number): Promise<JSConnection>  => {
 	const credentials = await func.getCredentials('natsApi', idx)
-	const nats = await connect(credentials)
-	const jsOptions = {
+	const options = natsConnectionOptions(credentials)
+	const nats = await connect(options)
+	const jsOptions:JetStreamOptions = {
 		apiPrefix: credentials['jsApiPrefix'] as string,
     timeout: credentials['jsTimeout'] as number,
     domain: credentials['jsDomain'] as string
-	} as JetStreamOptions;
+	}
 	if (jsOptions.apiPrefix === '') {
 		jsOptions.apiPrefix = undefined
 	}
 	if (jsOptions.domain === '') {
 		jsOptions.domain = undefined
 	}
-	return { nats, js: nats.jetstream() }
+
+
+	return { nats, js: nats.jetstream(defaultJsOptions(jsOptions)) }
 }
--- nodes/common.ts
+++ nodes/common.ts
@@ -1,9 +1,50 @@
-import { ICredentialTestFunctions, ICredentialsDecrypted, INodeCredentialTestResult } from "n8n-workflow";
-import { connect } from "nats";
+import { ICredentialDataDecryptedObject, ICredentialTestFunctions, ICredentialsDecrypted, INodeCredentialTestResult } from "n8n-workflow";
+import { Authenticator, ConnectionOptions, connect, credsAuthenticator, jwtAuthenticator, nkeyAuthenticator, tokenAuthenticator, usernamePasswordAuthenticator } from "nats";
+
+export function natsConnectionOptions(credentials: ICredentialDataDecryptedObject): ConnectionOptions {
+	const { user, pass, token, seed, jwtSeed, jwt, creds, tlsCa, tlsCert, tlsKey, ...options } = credentials
+	const authenticators: Authenticator[] = []
+
+	if (user && (user as string).length > 0) {
+		authenticators.push(usernamePasswordAuthenticator(user as string, pass && (pass as string).length > 0 ? pass as string : undefined))
+	}
+
+	if (token && (token as string).length > 0) {
+		authenticators.push(tokenAuthenticator(token as string))
+	}
+
+	if (seed && (seed as string).length > 0) {
+		authenticators.push(nkeyAuthenticator(new TextEncoder().encode(seed as string)))
+	}
+
+	if (jwt && (jwt as string).length > 0) {
+		authenticators.push(jwtAuthenticator(jwt as string, (jwtSeed && (jwtSeed as string).length > 0) ? new TextEncoder().encode(jwtSeed as string) : undefined))
+	}
+
+	if (creds && (creds as string).length > 0) {
+		authenticators.push(credsAuthenticator(new TextEncoder().encode(creds as string)))
+	}
+
+	if (tlsCa || tlsCert || tlsKey) {
+		options.tls = { ca: tlsCa, cert: tlsCert, key: tlsKey }
+	}
+
+	return { ...options, authenticator: authenticators }
+}
 
 export async function natsCredTest(this: ICredentialTestFunctions, credential: ICredentialsDecrypted): Promise<INodeCredentialTestResult> {
+	if(!credential.data) {
+		return {
+			status: 'Error',
+			message: `Credential data is required`,
+		}
+	}
+
 	try {
-		await (await connect(credential.data)).rtt()
+		const options = natsConnectionOptions(credential.data)
+		const nats = await connect(options)
+		await nats.rtt()
+		await nats.close()
 	} catch (error) {
 		return {
 			status: 'Error',
